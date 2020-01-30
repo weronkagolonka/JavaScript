6922a7ecf6e7d4e3dc09456fc1bbb58bf6ce78c0
Weronka Golonka <54840864+weronkagolonka@users.noreply.github.com>
2020-01-30 11:54:31 +0100

    Forkify app
    Search for your favourite recipes and add them to the list.
    
    A project which is a part of JavaScript course created by Jonas Schmedtmann.   Javascript with Webpack and Babel.

---
+++ starter/src/js/index.js
@@ -0,0 +1,22 @@
+// Global app controller
+//2 types of json pack - libraries, frameworks etc.; development tools
+
+// https://forkify-api.herokuapp.com/api/search
+
+/*
+https://crossorigin.me/
+another proxy if crossorigin doesn't work:
+https://cors-anywhere.herokuapp.com
+and it didn't sork
+*/
+
+import axios from 'axios';
+
+async function getResult(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+}
+getResult('pizza');
+
